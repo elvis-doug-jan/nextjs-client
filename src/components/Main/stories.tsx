6c0ce5a09c8d0fddfd0ae7f39203257eafe9306d
Elvis Douglas Janegitz <edjelvis@gmail.com>
2020-08-04 22:51:11 -0300

    Concluindo boilerplate

--- src/components/Main/stories.tsx
+++ src/components/Main/stories.tsx
@@ -1,8 +1,15 @@
+import { withKnobs, text } from '@storybook/addon-knobs'
 import Main from '.'
 
 export default {
-  title: Main,
-  component: Main
+  title: 'Main',
+  component: Main,
+  decorators: [withKnobs]
 }
 
-export const Basic = () => <Main />
+export const Basic = () => (
+  <Main
+    title={text('Title', 'React Avançado')}
+    description={text('Description', 'TypeScript, ReactJS, NextJS e Styled Components')}
+  />
+)
